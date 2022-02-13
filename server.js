8631eb2db6850156e5cb1c50a37b35f30033440c
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-02-14 00:04:59 +0600

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const https=require("https");
 const app=express();
 const nasa_api_key="hDBqoLGoHv3ONAVjOqFhIUcwDoTmxEPRoG8maWt0";
 
+// use the express-static middleware
+app.use(express.static("public"))
+
 app.get("/apod", (req, res) => {
     const url="https://api.nasa.gov/planetary/apod?api_key="+nasa_api_key;
     https.get(url, function(response){
@@ -24,6 +27,6 @@ app.get("/apod", (req, res) => {
     });
   });
 
-app.listen(port, () => {
-    console.log(`listening at${port}`)
-})
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
