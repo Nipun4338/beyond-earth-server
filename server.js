7a88e8516ff833009269835984f78c01677ff916
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-02-14 13:53:51 +0600

    only apod

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express=require("express");
 const https=require("https");
+var cors = require('cors')
 //const bodyparser=require("body-parser");
 const app=express();
 const nasa_api_key="hDBqoLGoHv3ONAVjOqFhIUcwDoTmxEPRoG8maWt0";
 
-// use the express-static middleware
-app.use(express.static("public"))
+app.use(cors())
 
 app.get("/apod", (req, res) => {
     const url="https://api.nasa.gov/planetary/apod?api_key="+nasa_api_key;
@@ -16,14 +16,14 @@ app.get("/apod", (req, res) => {
     });
   });
 
-  /*app.get("/eonet_events", (req, res) => {
+  app.get("/eonet_events", (req, res) => {
     const url="https://eonet.sci.gsfc.nasa.gov/api/v3/events?days=30";
     https.get(url, function(response){
         response.on("data", function(data){
             res.send(data);
         })
     });
-  });*/
+  });
 
 // start the server listening for requests
 app.listen(process.env.PORT || 4000, 
