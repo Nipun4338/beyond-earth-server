91c5a4473705b6fbc2609b9cce45680baa977fce
Nipun Paul <49658560+Nipun4338@users.noreply.github.com>
2022-02-18 00:00:50 +0600

    api added'

--- server.js
+++ server.js
@@ -11,15 +11,40 @@ app.get("/apod", (req, res) => {
     const url="https://api.nasa.gov/planetary/apod?api_key="+nasa_api_key;
     https.get(url, function(response){
         response.on("data", function(data){
+            /*var json=JSON.parse(data);
+            console.log(json);
+            res.json(json);*/
             res.send(data);
         })
     });
   });
 
-  app.get("/eonet_events", (req, res) => {
-    const url="https://eonet.sci.gsfc.nasa.gov/api/v3/events?days=30";
+  app.get("/known_count", (req, res) => {
+    const url="https://api.le-systeme-solaire.net/rest/knowncount/";
     https.get(url, function(response){
         response.on("data", function(data){
+            /*const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+            console.log('Status Code:', res.statusCode);
+            console.log('Date in Response header:', headerDate);
+            var json=JSON.parse(data);
+            console.log(json);
+            res.json(json);*/
+            res.send(data);
+        })
+    });
+  });
+
+  app.get("/bodies", (req, res) => {
+    const url="http://localhost:4000/bodies/";
+    https.get(url, function(response){
+        response.on("data", function(data){
+            /*const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+            console.log('Status Code:', res.statusCode);
+            console.log('Date in Response header:', headerDate);
+            var json=JSON.parse(data);
+            console.log(json);
+            res.json(json);*/
+            
             res.send(data);
         })
     });
